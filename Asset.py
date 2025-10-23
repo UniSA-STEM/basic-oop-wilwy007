51108694eeb1a94b2e26b620b61c172df46780be
wilwy007 <scott.willoughby@rtwsa.com>
2025-10-24 09:25:13 +1030

    Added the Asset Class with string representation and encryption flag

--- Asset.py
+++ Asset.py
@@ -1,8 +1,24 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: The Asset Class represents the digital assets used in the simulation
+    and each asset has a name, description and encrypted boolean flag.
+Author: William Willoughby
+ID: 110477792
+Username: wilwy007
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+class Asset:
+    def __init__(self, name, description, encrypted: bool = False):
+        self.name = name
+        self.description = description
+        self.encrypted = encrypted
+    def __str__(self) -> str:
+        """Return a string representation of the Asset."""
+        base = f"{self.name}: {self.description}"
+        return f"{base} [Encrypted]" if self.encrypted else base
+    def encrypt(self):
+        """Encrypt the Asset."""
+        self.encrypted = True
+    def decrypt(self):
+        """Decrypt the Asset."""
+        self.encrypted = False
