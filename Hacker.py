7c24683f44083d6c7c2d6f404e8334515d566c74
wilwy007 <scott.willoughby@rtwsa.com>
2025-10-24 14:50:23 +1030

    Added Asset and Inventory control functions

--- Hacker.py
+++ Hacker.py
@@ -26,3 +26,29 @@ class Hacker:
         inv = ", ".join([str(a) for a in self._inventory]) or "Empty"
         rig_name = self.rig.name if self.rig else "No Rig"
         return f"Hacker: {self.name} | Rig: {rig_name} | Trace: {self.trace_level} | Inventory: [{inv}]"
+
+def add_to_inventory(self, asset: Asset):
+    """Add an asset to inventory."""
+    self._inventory.append(asset)
+    print(f"{self,name}: Added {asset.name} to inventory.")
+
+def scan_inventory_for(self, name: str) -> Optional[Asset]:
+    """
+    Remove and return the first asset in inventory with a matching name
+    if not found return None.
+    """
+    for i, a in enumerate(self._inventory):
+        if a.name == name:
+            return self._inventory.pop(i)
+    return None
+
+def has_asset(self, name: str) -> bool:
+    return any(a.name == name for a in self._inventory)
+
+def _consume_by_name(self, name: str) -> bool:
+    """consume an asset (used as consumer_callable for Rig.repair)."""
+    a = self.scan_inventory_for(name)
+    if a:
+        print(f"{self,name}: Consumed {name}.")
+        return True
+    return False
