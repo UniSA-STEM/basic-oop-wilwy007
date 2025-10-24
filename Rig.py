52eef083b68e9dbb6a773d40407c33a8ab21df8b
wilwy007 <scott.willoughby@rtwsa.com>
2025-10-24 10:35:48 +1030

    Set up imports, added the Rig Class with initial values.

--- Rig.py
+++ Rig.py
@@ -1,8 +1,25 @@
 """
 File: Rig.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: The Rig Class represents the Hackers computer. It handles storage, damage,
+    broken state, upgrades and repairs. Each rig starts with two data Spikes and one
+    removable drive.
+Author: William Willoughby
+ID: 110477792
+Username: wilwy007
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+from typing import List
+import random
+from Asset import Asset
+
+class Rig:
+    def __init__(self, name: str):
+        self.name = name
+        self.damage = 0
+        self.broken = False
+        self.storage: List[Asset] = []
+        self.upgrade_level = 0
+
+        self.storage.append(Asset("Data Spike", "For use in battles."))
+        self.storage.append(Asset("Data Spike", "For use in battles."))
+        self.storage.append(Asset("Removable Drive", "Found in rigs and used for extraction."))
