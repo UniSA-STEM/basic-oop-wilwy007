c9fe83ccb6a18c1734b93b3964b62930981ff083
wilwy007 <scott.willoughby@rtwsa.com>
2025-10-24 13:14:14 +1030

    Added functions for Generating, Storing and Releasing Assets

--- Rig.py
+++ Rig.py
@@ -68,3 +68,45 @@ def upgrade(self):
         self.broken = False
     print(f"{self.name}: Is now upgraded to level {self.upgrade_level}.")
 
+def generate_assets(self) -> Asset:
+    """
+    Generate one asset randomly. if storage has room store  it and return asset
+    else if storage full return asset but do not store.
+    """
+    choices = [
+        Asset("Data Spike", "For use in battles."),
+        Asset("Removable Drive", "Found in rigs and used for extraction."),
+        Asset("CryptoToken", "Used to acquire or repair rigs."),
+        Asset("Security Chip", "Used to encrypt or decrypt assets"),
+        Asset("Hardware Patch", "Used to upgrade rigs")
+    ]
+    generated = random.choice(choices)
+    if len(self.storage) < self.storage_capacity():
+        self.storage.append(generated)
+        print(f"{self.name}: generated {generated} asset and stored in storage.")
+    else:
+        print(f"{self.name}: Generated {generated} but storage is full.")
+    return generated
+
+def store_asset(self, asset: Asset) -> bool:
+    """Store asset in storage if storage not full."""
+    if len(self.storage) >= self.storage_capacity():
+        print(f"{self.name}: Cannot store {asset} as storage is full.")
+        return False
+    self.storage.append(asset)
+    print(f"{self.name}: Stored {asset} in storage.")
+    return True
+
+def release_asset(self, asset_name: str) -> Asset or None:
+    """
+    Release the first non-encrypted asset with matching name.
+    If the asset is not encrypted, return None.
+    """
+    for i, a in enumerate(self.storage):
+        if a.name == asset_name:
+            if a.encrypted:
+                print(f"{self.name}: {asset_name} is encrypted and cannot be released.")
+                return None
+            return self.storage.pop(i)
+    print(f"{self.name}: Asset {asset_name} not found.")
+    return None
